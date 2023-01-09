c90ac6c0be1895859ed139bfa01973941f65f3c8
KiranKumarMalik <malikkiran413@gmail.com>
2023-01-09 13:44:21 +0530

    My new portfolio website

---
+++ mylogin/logout.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    if(isset($_SESSION['email']))
+    {
+        session_destroy();
+        header('location:../login/login.php');
+    }
+    else
+    {
+        header('location:../login/login.php');
+    }
+?>
